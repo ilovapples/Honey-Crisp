9e9143eec1f2b8fe98e488ddccea9e6b9d9cc460
ilovapples <ashergrossman185@gmail.com>
2023-01-08 14:51:58 -0600

    Modified the convert.py file
    Spaced out a couple of the lines and removed some placeholder text.

--- math/convert.py
+++ math/convert.py
@@ -1,8 +1,11 @@
 from PIL import Image
 def image(path, TO_png_jpg_webp):
     FROM_png_jpg_webp = path[path.rindex('.')+1:]
+    
     newPath = path.replace(FROM_png_jpg_webp, TO_png_jpg_webp)
+    
     image = Image.open(path).convert("RGB")
+    
     if FROM_png_jpg_webp == "png":
         if TO_png_jpg_webp == "jpg":
             image.save(newPath, TO_png_jpg_webp)
@@ -18,8 +21,5 @@ def image(path, TO_png_jpg_webp):
             image.save(newPath, TO_png_jpg_webp)
         elif TO_png_jpg_webp == "jpg":
             image.save(newPath, TO_png_jpg_webp)
-'''      
- /  .  P  N  G
--5 -4 -3 -2 -1
-'''
-image("D:\Programming Jokes\y = y + 1.png", "webp")
+
+image(input("Path to image: "), input("File type to convert to: "))
